330d1529ce8301fe369e727b79367f08e4e1cbc9
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-10-05 18:42:55 -0500

    convert to reactjs

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import App from './Home';
+import Home from './Home/index';
 import Admin from './Admin';
 function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={App} />
+                <Route path="/" exact component={Home} />
                 <Route path="/admin" exact component={Admin} />
-                <Route component={App} />
+                <Route component={Home} />
             </Switch>
         </BrowserRouter>
     );
