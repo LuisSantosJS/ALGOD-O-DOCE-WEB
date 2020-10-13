d1885012f5789f374272030a41c7b411a05ae5e7
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-10-13 00:32:13 -0500

    finish

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,151 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './styles.css';
+import api, { URL } from '../../service/api';
+import Header from '../../components/Header';
+import Modal from 'react-modal';
+import { useToasts } from 'react-toast-notifications';
+import { useToken } from '../../context/contextMain';
+// @ts-ignore
+import { mask } from 'remask'
+import upload, { UPLOAD_URL } from '../../service/upload'
+interface Item {
+    _id: string,
+    description: string,
+    name: string,
+    date: string,
 
+}
+const del = require('../../assets/delete.png');
+const Add = require('../../assets/add-image.png');
 const Cardapio: React.FC = () => {
+    const [result, setResult] = useState<Item[]>([]);
+    const [modal, setModal] = useState<boolean>(false);
+    const { token } = useToken();
+
+    const { addToast } = useToasts();
+ 
+    const [desc, setDesc] = useState<string>('');
+    const [name, setName] = useState<string>('');
+    const [date, setDate] = useState<string>('');
+
+    useEffect(() => {
+        api.get('/cardapio/index').then(res => {
+            setResult(res.data);
+        })
+    }, []);
+
+    const onSubmit = () => {
+        const config = {
+            headers: {
+                'x-access-token': `${token}`
+            }
+        }
+        api.post('/cardapio/create', {
+            description: String(desc),
+            name: String(name),
+            date: String(date),
+
+        }, config).then(res => {
+            console.log(res.data)
+            const data: Item = res.data;
+            setResult([...result, data]);
+            setModal(false);
+            addToast(`Imagem enviada com sucesso!`, {
+                appearance: 'success',
+                autoDismiss: true,
+            })
+        }).catch(() => {
+            addToast(`Ocorreu um erro!`, {
+                appearance: 'error',
+                autoDismiss: true,
+            })
+        })
+
+    }
+
+    const onDelete = (id: string) => {
+        const config = {
+            headers: {
+                'x-access-token': `${token}`
+            }
+        }
+        api.post('/cardapio/delete', { id: id }, config).then(res => {
+            if (res.data.message === 'error') {
+                addToast(`Ocorreu um erro!`, {
+                    appearance: 'error',
+                    autoDismiss: true,
+                })
+            } else {
+                const valor = result.filter(res => res._id !== id);
+                setResult(valor);
+
+                addToast(`Sucesso`, {
+                    appearance: 'success',
+                    autoDismiss: true,
+                })
+
+            }
+        })
+
+
+    }
+    const customStyles = {
+
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)'
+        }
+    };
+
     return (
         <>
-            <div className="viewContainer">
+            <Header />
+            <main id="main">
+                <div className="spacingviewbash" />
 
+                <ul>
+                    {result.map(res => {
+                        return (
+                            <React.Fragment key={res._id}>
+                                <div className='vauibvusir' >
+                                    <div className='cbakusrycvskV'>
+                                        <h2 className='fnvisunacinsprvs'>{res.name}</h2>
+                                        <h5 className='fnvisunacinsprvs'>{res.description}</h5>
+                                        <h6 className='fnvisunacinsprvs'>{res.date}</h6>
+                                    </div>
+                                    <img onClick={() => onDelete(res._id)} className='avptiuytdefghjytr' height='50' src={del} alt="" />
+                                </div>
+                                <br />
+                                <br />
+                            </React.Fragment>
+                        )
+                    })}
+                </ul>
+
+            </main>
+            <Modal
+                isOpen={modal}
+                onRequestClose={() => setModal(false)}
+                style={customStyles}
+                appElement={document.getElementById('root') as HTMLElement}
+                contentLabel="Form Modal">
+                <form onSubmit={onSubmit} className='formsss' encType='multipart/form-data'>
+                    <div className='rowss' >
+                        {/* <label htmlFor="lname"> Descrição</label> */}
+                        <input className={'inpstshome'} value={name} onChange={(e) => setName(e.target.value)} placeholder="Insira um nome" type="text" />
+                        <input className={'inpstshome'} value={desc} onChange={(e) => setDesc(e.target.value)} placeholder="Insira uma descrição" type="text" />
+                        <input className={'inpstshome'} value={date} onChange={(e) => setDate(mask(e.target.value, '99/99/9999'))} placeholder="Insira a data" type="text" />
+                    </div>
+    
+                    <strong onClick={onSubmit} className='vjanltjviurytrhbnkc' >Fazer Upload</strong>
+                </form>
+            </Modal>
+            <div onClick={() => setModal(true)} className="float">
+                <img height='50%' src={Add} alt="" />
             </div>
         </>
     )
--- src/pages/Professores/index.tsx
+++ src/pages/Professores/index.tsx
@@ -1,10 +1,128 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import Header from '../../components/Header';
+import api from '../../service/api';
+import { useToasts } from 'react-toast-notifications';
+import { useToken } from '../../context/contextMain';
+import Modal from 'react-modal';
+import './styles.css';
+interface Item {
+    _id: string,
+    name: string,
+    email: string,
+}
+const del = require('../../assets/delete.png');
+const Add = require('../../assets/add-image.png');
 const Professores: React.FC = () => {
+    const [result, setResult] = useState<Item[]>([]);
+    const [modal, setModal] = useState<boolean>(false);
+    const [name, setName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [code, setCode] = useState<string>('');
+    const { token } = useToken();
+    const { addToast } = useToasts();
+    useEffect(() => {
+        api.get('/users/index').then(res => {
+            setResult(res.data);
+        })
+    }, []);
+    const customStyles = {
+
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)'
+        }
+    };
+    const onSubmit = () => {
+        const config = {
+            headers: {
+                'x-access-token': `${token}`
+            }
+        }
+        api.post('/users/create', {
+            email,
+            password: code,
+            name
+        },config).then((res) => {
+            setResult([...result, res.data])
+            setModal(true)
+        })
+    }
+
+    const onDelete = (id: string) => {
+        const config = {
+            headers: {
+                'x-access-token': `${token}`
+            }
+        }
+        api.post('/users/delete', { id: id }, config).then(res => {
+            if (res.data.message === 'error') {
+                addToast(`Ocorreu um erro!`, {
+                    appearance: 'error',
+                    autoDismiss: true,
+                })
+            } else {
+                const valor = result.filter(res => res._id !== id);
+                setResult(valor);
+
+                addToast(`Sucesso`, {
+                    appearance: 'success',
+                    autoDismiss: true,
+                })
+
+            }
+        })
+
+    }
+
     return (
         <>
-            <div className="viewContainer">
+            <Header />
+            <main id="main">
+                <div className="viewContainercsrS">
+                    <div className="spacingviewbash" />
+                    <ul>
+                        {result.map(res => {
+                            return (
+                                <React.Fragment key={res._id}>
+                                    <div className='abatdyuterty'  >
+                                        <div className='carajhghjkcasrv' >
+                                            <h3 className='vstrert'>{res.name}</h3>
+                                            <h6 className='vstrert'>{res.email}</h6>
+                                        </div>
 
+                                        <img src={del} onClick={() => onDelete(res._id)} className='as' height='40' alt="delete" />
+
+                                    </div>
+                                    <br />
+                                    <br />
+                                </React.Fragment>
+                            )
+                        })}
+                    </ul>
+                </div>
+            </main>
+            <Modal
+                isOpen={modal}
+                onRequestClose={() => setModal(false)}
+                style={customStyles}
+                appElement={document.getElementById('root') as HTMLElement}
+                contentLabel="Form Modal">
+                <form onSubmit={onSubmit} className='formsss' encType='multipart/form-data'>
+                    <div className='rowss' >
+                        {/* <label htmlFor="lname"> Descrição</label> */}
+                        <input className={'inpstshome'} value={name} onChange={(e) => setName(e.target.value)} placeholder="Insira um nome˜" type="text" />
+                        <input className={'inpstshome'} value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Insira um email" type="text" />
+                        <input className={'inpstshome'} value={code} onChange={(e) => setCode(e.target.value)} placeholder="Insira uma senha" type="text" />
+                    </div>
+                    <strong onClick={onSubmit} className='vjanltjviurytrhbnkc' >Fazer Upload</strong>
+                </form>
+            </Modal>
+            <div onClick={() => setModal(true)} className="float">
+                <img height='50%' src={Add} alt="" />
             </div>
         </>
     )
